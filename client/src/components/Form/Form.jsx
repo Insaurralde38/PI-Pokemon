74c4e3914234ace529d8c6e6da16d5bcddd37a65
diego insaurralde <djinsaurralde38@gmail.com>
2023-08-29 13:57:03 -0300

    PokeApi (PI Henry)

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -87,26 +87,26 @@ function Form() {
   return (
     <div className="create-section">
       <form className="form-cont" onSubmit={handleSubmit} encType="multipart/form-data">
-        <h2>CREATE YOUR OWN POKEMON</h2>
-        <label>Name:</label>
-          <input type="text" name="name" value={form.name} onChange={handleChange}></input>
+        <h2 className="form-title">CREATE YOUR OWN POKEMON</h2>
+        <label className="form-label" >Name:</label>
+          <input type="text" name="name" value={form.name} onChange={handleChange} className="form-input"></input>
           {error.name && <span>{error.name}</span>}
         {/* <label>image: </label>
           <input type="file" name="image" onChange={handleFileChange} accept=".jpg, .png" /> */}
-        <label>Hit Points: {form.hp}</label>
+        <label className="form-label" >Hit Points: {form.hp}</label>
           <input type="range" min="1" max="255" name="hp" value={form.hp} onChange={handleChange}></input>
-        <label>Attack: {form.attack}</label>
+        <label className="form-label" >Attack: {form.attack}</label>
           <input type="range" min="1" max="190" name="attack" value={form.attack} onChange={handleChange}></input>
-        <label>Defense: {form.defense}</label>
+        <label className="form-label" >Defense: {form.defense}</label>
           <input type="range" min="1" max="250" name="defense" value={form.defense} onChange={handleChange}></input>
-        <label>Speed: {form.speed}</label>
+        <label className="form-label" >Speed: {form.speed}</label>
           <input type="range" min="1" max="200" name="speed" value={form.speed} onChange={handleChange}></input>
-        <label>Height: {form.height / 10} Mts.</label>
+        <label className="form-label" >Height: {form.height / 10} Mts.</label>
           <input type="range" min="1" max="200" name="height" value={form.height} onChange={handleChange}></input>
-        <label>Weight: {form.weight / 10} Kgs.</label>
+        <label className="form-label" >Weight: {form.weight / 10} Kgs.</label>
           <input type="range" min="1" max="9999" name="weight" value={form.weight} onChange={handleChange}></input>
-        <label>Types:</label>
-          <select className="select-button" name="type" onChange={handleSelect}>
+        <label className="form-label" >Types:</label>
+          <select className="form-dropdown" name="type" onChange={handleSelect}>
             {types
               .sort((a, b) => a.name.localeCompare(b.name))
               .map((type) => ( <option key={type.id} value={type.name}>{type.name}</option> ))}
@@ -116,11 +116,11 @@ function Form() {
             const cardLogo = typeLogo[type]
             return (
               <span key={type} className="delete-type-cont">
-                <img className="type" src={cardLogo}  onClick={() => handleDelete(type)} alt="" />
+                <img className="form-type" src={cardLogo}  onClick={() => handleDelete(type)} alt="" />
               </span>
             )})}
         </div>
-        <button type="submit" className="createpoke-button">Create Pokemon</button>
+        <button type="submit" className="form-button">CREATE POKÉMON</button>
       </form>
     </div>
   );
